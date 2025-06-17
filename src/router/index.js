176fec9a3baa5154af2b67d4e845e4666a64eb84
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-06-17 13:35:51 -0400

    0.746

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { supabase } from '@/supabaseClient';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
