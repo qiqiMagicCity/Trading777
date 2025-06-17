0e392316c3f09f31b811303b9a16b99779b8364f
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-06-17 00:05:30 -0400

    0.71

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
-export default {
-  auth: {
-    signInWithPassword: () => {},
-    signUp: () => {},
-  }
-}
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://zcosfwmtatuheqrytvad.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5...'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
