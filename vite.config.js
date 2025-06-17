71e32b1b91f035d8d840a095f7fc6a72f0762315
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-06-17 18:04:33 -0400

    1.0.1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   define: {
-    __APP_VERSION__: JSON.stringify('1.0.0')
+    __APP_VERSION__: JSON.stringify('1.0.1')
   }
 });
