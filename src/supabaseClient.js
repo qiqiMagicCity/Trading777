52180f96643c3971b91bbc9ee0f4b7fc750bfea8
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-06-18 10:48:20 -0400

    1.1.1

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
-const url = import.meta.env.VITE_SUPABASE_URL;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
-if(!url || !key){
-  console.warn('Supabase env vars missing!');
-}
-export const supabase = (url && key) ? createClient(url,key) : { auth:{ getSession:async()=>({data:{session:null}}) }, from:()=>({ insert:async()=>({}) }) };
+export const supabase = { auth:{ getSession: async()=>({data:{session:null}}) } }
