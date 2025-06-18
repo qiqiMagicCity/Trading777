5d8bb8035c06e312cf55e202e8416afc57f2de61
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-06-18 00:07:19 -0400

    1.0.3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
   <div class="footer">
     本站功能逐步完善中，敬请期待。对本站感兴趣的可以联系站长共同创建<br/>
     © 魔都万事屋™ 2005 – 2025 版权所有 • 保留所有权利 • MagicCity Global Tec<br/>
-    版本 v1.0.2
+    版本 v1.0.3
   </div>
 <style scoped>
 .footer {
