3d0861a68b5482b5516a5f1065948a86a547035c
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-06-20 02:50:27 -0400

    1.2.6

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -23,7 +23,7 @@
   <span class="grey">本站功能逐步完善中，敬请期待。对本站感兴趣的可以联系站长共同创建</span>
   <span class="green">© 魔都万事屋™</span>
   <span class="green">2005 – 2025 版权所有 • 保留所有权利 • MagicCity Global Tec</span>
-  <span class="green">版本 v1.2.5</span>
+  <span class="green">版本 v1.2.6</span>
 </div>
 </template>
 
@@ -144,14 +144,8 @@ function edit(row) {
 
 
 
-function edit(row) {
-  router.push({ path: '/record', query: { editId: row.id } });
-}
 
 
-function edit(row){
-  router.push({ path: '/record', query: { editId: row.id } });
-}
 
 </script>
 
