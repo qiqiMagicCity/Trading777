14cfd700d9aeff4f28cd5badaa851da9f641f4ff
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-06-20 06:53:57 -0400

    1.3.8

--- src/views/ProfitList.vue
+++ src/views/ProfitList.vue
@@ -14,7 +14,13 @@
       </tbody>
     </table>
   </div>
-</template>
+  <div class="footer">
+    <div class="footer-line">本站功能逐步完善中，敬请期待。对本站感兴趣的可以联系站长共同创建</div>
+    <div class="footer-line">© 魔都万事屋™</div>
+    <div class="footer-line">2005 – 2025 版权所有 • 保留所有权利 • MagicCity Global Tec</div>
+    <div class="footer-line">版本 v1.3.8</div>
+  </div>
+</template></template>
 <script setup>
 import { ref, onMounted } from 'vue';
 import { supabase } from '@/supabaseClient';
