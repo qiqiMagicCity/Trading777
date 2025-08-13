d95725fa119fe95db7f1089670f203263ab73d28
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-08-12 23:04:56 -0400

    test: map @ alias for Jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.spec.ts'],
-  transform: { '^.+\\.ts$': ['ts-jest', {}] }
+  transform: { '^.+\\.ts$': ['ts-jest', {}] },
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/apps/web/app/$1' }
 };
 export default config;
