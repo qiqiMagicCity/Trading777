afffddccd41e975ccf37fe99d853e2d969d0692e
qiqiMagicCity <QiQi_MagicCity@outlook.com>
2025-08-12 23:47:26 -0400

    Expand Jest testMatch patterns

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.spec.ts'],
+  testMatch: ['**/tests/**/*.spec.ts', '**/app/lib/**/*.(test|spec).ts'],
   transform: { '^.+\\.ts$': ['ts-jest', {}] }
 };
 export default config;
